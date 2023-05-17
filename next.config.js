2fcbf923c2a22589f4a234423c1e95fe2877b95a
marcosguilhermef <marcosguilhermefh@gmail.com>
2023-05-16 22:29:28 -0300

    ajuste

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextBuildId = require('next-build-id')
+
+const nextConfig = {
+    generateBuildId: () => nextBuildId({ dir: __dirname, describe: true})
+}
 
 module.exports = nextConfig
--- src/app/Compoments/Layout/layout/Layout.tsx
+++ src/app/Compoments/Layout/layout/Layout.tsx
@@ -1,7 +1,12 @@
 import Header from "@/app/Compoments/Layout/header";
 import Foot from '@/app/Compoments/Layout/foot'
 import Body from '@/app/Compoments/Layout/body'
+import ga4 from "react-ga4";
 export default function Layout({children, title = 'Tools For Devs'}: { children: React.ReactNode, title:string}) {
+
+    ga4.initialize("G-C6W5KW8XSJ")
+
+
     return(
         <html lang="pt-BR">
             <head>
