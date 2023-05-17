da485a4b7da43745a7a7236bf53723f16febac6d
marcosguilhermef <marcosguilhermefh@gmail.com>
2023-05-16 21:24:09 -0300

    google analytics

--- src/app/Compoments/Layout/header/index.jsx
+++ src/app/Compoments/Layout/header/index.jsx
@@ -7,7 +7,6 @@ import ga4 from 'react-ga4';
 export default function Header() {
   ga4.initialize("G-C6W5KW8XSJ")
   ga4.send({ hitType: "pageview", page: window.location.href });
-
   return (
     <header>
         <Navbar expand='lg' className="mb-3 navbar-color" >
