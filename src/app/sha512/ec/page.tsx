3c7481bc848fcdd99796015c13acb0ed4bd55612
marcosguilhermef <marcosguilhermefh@gmail.com>
2023-05-26 02:56:36 -0300

    ajuste seo, detectar IP

--- src/app/sha512/ec/page.tsx
+++ src/app/sha512/ec/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
     openGraph: {
        "title": "Gerador de Hash SHA512 para Texto - ToolsForDev.com.br",
        "description": "Esta ferramenta online permite converter texto em um hash SHA512, que é um algoritmo de criptografia robusto e altamente seguro. Com a conversão para SHA512, você pode proteger senhas e informações confidenciais de maneira avançada. Simplifique o processo de geração de hashes SHA512 com esta ferramenta eficiente.",
-       url: 'https://toolsfordevs.com.br',
+       url: 'https://toolsfordevs.com.br/sha512/ec',
        images: [
            {
               url: "https://toolsfordevs.com.br/sha512.png",
