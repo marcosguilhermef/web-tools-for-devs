8f219d67eaee207c0d510bfe7fb8dbed7af3b698
marcosguilhermef <marcosguilhermefh@gmail.com>
2023-05-31 14:38:42 -0300

    ajustes no SEO

--- src/app/sha224/ec/Component.tsx
+++ src/app/sha224/ec/Component.tsx
@@ -19,9 +19,9 @@ export default function Component({ h1 }: { h1?: string }) {
                     O SHA-224 é uma variante do SHA-256 que produz um valor de hash de 224 bits (28 bytes). Ele foi projetado para ser usado como uma alternativa menor ao SHA-256 em sistemas com restrições de recursos. O SHA-224 fornece um nível moderado de segurança e é uma boa escolha quando um valor de hash mais curto é necessário, mas o SHA-256 não é viável devido a limitações de desempenho ou armazenamento.
                 </p>   
             </Articles>
-            <Articles title={"O que é faz o SHA3-512 Encrypt (codificador SHA3-512)?"}>
+            <Articles title={"O que é faz o SHA-224 Encrypt (codificador SHA224)?"}>
                 <p>
-                Um codificador SHA3-512, ou Encrypt SHA3-512, é um algoritmo de criptografia que converte uma entrada de dados em um hash de 512 bits. Ele fornece segurança e confidencialidade aos dados. O SHA3-512 gera uma sequência única de caracteres que não pode ser revertida, permitindo a verificação de integridade dos dados sem expor informações sensíveis. É usado em aplicações que exigem proteção e autenticação de dados críticos, como armazenamento seguro de senhas, assinaturas digitais e autenticação de mensagens. O algoritmo SHA3-512 é considerado seguro e resistente a ataques criptográficos.
+                Um codificador SHA224, ou Encrypt SHA224, é um algoritmo de criptografia que converte uma entrada de dados em um hash de 224 bits. Ele é usado para garantir a integridade e autenticidade dos dados. O SHA224 gera uma sequência única de caracteres que não pode ser revertida, permitindo a verificação de dados sem expor informações sensíveis. É usado em aplicações que exigem proteção e autenticação de dados, como assinaturas digitais, autenticação de mensagens e armazenamento seguro de senhas. O algoritmo SHA224 é amplamente utilizado e considerado seguro para criptografia e integridade de dados.
                 </p>
             </Articles>
         </div>
