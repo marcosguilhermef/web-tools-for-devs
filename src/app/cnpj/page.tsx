7d7a482776dc4b1fa525ddffe095edccd58c4fd6
marcosguilhermef <marcosguilhermefh@gmail.com>
2023-06-03 23:23:03 -0300

    ajuste

--- src/app/cnpj/page.tsx
+++ src/app/cnpj/page.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
      title: 'Validador de cnpj - ToolsForDev.com.br',
      description: "Verifique a autenticidade de CNPJs com nossa ferramenta de validação. Garanta a integridade dos números de registro e evite problemas fiscais e comerciais.",
      alternates: { 
-        canonical:"https://toolsfordevs.com.br/cpf"
+        canonical:"https://toolsfordevs.com.br/cnpj"
     },
      openGraph: {
         "title": "Validar de CPF - ToolsForDev.com.br",
